ef1708a43a91be5fe94403574decd21c23774c54
Steven Waller <jstevenwaller@gmail.com>
2018-07-31 11:29:07 -0700

    Import bootstrap components from src

--- webpack.config.js
+++ webpack.config.js
@@ -70,17 +70,17 @@ module.exports = {
     alias: {
       // Use .modernizrrc to create a custom build instead of using full library
       modernizr$: path.resolve(__dirname, '.modernizrrc'),
-      Alert: path.resolve(__dirname, 'node_modules/bootstrap/js/dist/alert.js'),
-      Button: path.resolve(__dirname, 'node_modules/bootstrap/js/dist/button.js'),
-      Carousel: path.resolve(__dirname, 'node_modules/bootstrap/js/dist/carousel.js'),
-      Collapse: path.resolve(__dirname, 'node_modules/bootstrap/js/dist/collapse.js'),
-      Dropdown: path.resolve(__dirname, 'node_modules/bootstrap/js/dist/dropdown.js'),
-      Modal: path.resolve(__dirname, 'node_modules/bootstrap/js/dist/modal.js'),
-      Popover: path.resolve(__dirname, 'node_modules/bootstrap/js/dist/popover.js'),
-      Scrollspy: path.resolve(__dirname, 'node_modules/bootstrap/js/dist/scrollspy.js'),
-      Tab: path.resolve(__dirname, 'node_modules/bootstrap/js/dist/tab.js'),
-      Tooltip: path.resolve(__dirname, 'node_modules/bootstrap/js/dist/tooltip.js'),
-      Util: path.resolve(__dirname, 'node_modules/bootstrap/js/dist/util.js')
+      Alert: path.resolve(__dirname, 'node_modules/bootstrap/js/src/alert.js'),
+      Button: path.resolve(__dirname, 'node_modules/bootstrap/js/src/button.js'),
+      Carousel: path.resolve(__dirname, 'node_modules/bootstrap/js/src/carousel.js'),
+      Collapse: path.resolve(__dirname, 'node_modules/bootstrap/js/src/collapse.js'),
+      Dropdown: path.resolve(__dirname, 'node_modules/bootstrap/js/src/dropdown.js'),
+      Modal: path.resolve(__dirname, 'node_modules/bootstrap/js/src/modal.js'),
+      Popover: path.resolve(__dirname, 'node_modules/bootstrap/js/src/popover.js'),
+      Scrollspy: path.resolve(__dirname, 'node_modules/bootstrap/js/src/scrollspy.js'),
+      Tab: path.resolve(__dirname, 'node_modules/bootstrap/js/src/tab.js'),
+      Tooltip: path.resolve(__dirname, 'node_modules/bootstrap/js/src/tooltip.js'),
+      Util: path.resolve(__dirname, 'node_modules/bootstrap/js/src/util.js')
     }
   },
 
