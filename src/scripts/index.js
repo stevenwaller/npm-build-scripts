f646c9d0c2788a1553277649f5ce6a55ecdbee79
Steven Waller <jstevenwaller@gmail.com>
2018-08-01 14:45:58 -0700

    Make Eslint happy

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
-import Modernizr from 'modernizr';
+import Modernizr from 'modernizr'; // eslint-disable-line no-unused-vars
 import $ from 'jquery';
 
 // Components
 import './example-component';
 
-
+// Example
+const headers = $('h1');
+console.log('Number of headers', headers.length);
